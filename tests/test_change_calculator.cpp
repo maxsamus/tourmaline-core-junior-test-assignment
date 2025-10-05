fe4a392ef5adcbd25d2b4d121089430d288bf2ac
Maxim Samusenko <maximsamusen@gmail.com>
2025-10-05 16:33:44 +0500

    test: add unit tests for make_change(); reveal failing behavior

--- tests/test_change_calculator.cpp
+++ tests/test_change_calculator.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "../src/change_calculator.h"
 
-TEST(ChangeCalculatorTests, CanMakeChange)
+TEST(ChangeCalculatorTests, CanMakeChange1)
 {
     std::vector<int> result;
 
@@ -12,6 +12,84 @@ TEST(ChangeCalculatorTests, CanMakeChange)
     bool success = make_change(values, counts, target, result);
     EXPECT_TRUE(success);
     EXPECT_EQ(result, std::vector<int>({3, 1}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange2)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {2};
+    std::vector<int> counts = {2};
+    int target = 4;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({2, 2}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange3)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {2, 3};
+    std::vector<int> counts = {3, 1};
+    int target = 6;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({2, 2, 2}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange4)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {1, 3, 4, 5};
+    std::vector<int> counts = {2, 1, 2, 1};
+    int target = 10;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({4, 4, 1, 1}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange5)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {1, 7, 10};
+    std::vector<int> counts = {3, 1, 1};
+    int target = 20;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({10, 7, 1, 1, 1}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange6)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {1, 17, 23, 29, 41, 50};
+    std::vector<int> counts = {2, 1, 3, 0, 1, 1};
+    int target = 112;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({41, 23, 23, 23, 1, 1}));
+}
+
+TEST(ChangeCalculatorTests, CanMakeChange7)
+{
+    std::vector<int> result;
+
+    std::vector<int> values = {1, 5, 13, 27, 44, 61};
+    std::vector<int> counts = {10, 1, 2, 3, 3, 1};
+    int target = 116;
+
+    bool success = make_change(values, counts, target, result);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(result, std::vector<int>({44, 44, 27, 1}));
 }
 
 TEST(ChangeCalculatorTests, CannotMakeChange)
