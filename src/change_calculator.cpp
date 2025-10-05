a60b34b27cc5ef2b3598ff30011d8c70555ffe84
Maxim Samusenko <maximsamusen@gmail.com>
2025-10-05 16:52:48 +0500

    feat: implement BFS-based solution for make_change function

--- src/change_calculator.cpp
+++ src/change_calculator.cpp
@@ -1,4 +1,5 @@
 #include "change_calculator.h"
+#include <queue>
 #include <algorithm>
 
 bool make_change(const std::vector<int> &values,
@@ -31,50 +32,49 @@ bool make_change(const std::vector<int> &values,
     if (target == 0)
         return true;
 
-    std::vector<int> dp(target + 1, -1);  // dp[s] stores the minimum number of coins for sum s
-    std::vector<int> used(target + 1, 0); // used[s] stores the index of the coin used for sum s
-    dp[0] = 0;
+    std::vector<bool> used(target + 1, false); // Track visited sums
+    std::queue<std::pair<int, std::vector<int>>> q;
 
-    for (int i = 0; i < n; i++)
+    used[0] = true;
+    q.emplace(0, std::vector<int>(n, 0));
+
+    while (!q.empty())
     {
-        for (int s = target; s >= 0; s--)
+        auto [sum, used_coins] = q.front();
+        q.pop();
+
+        if (sum == target)
         {
-            if (dp[s] != -1) // If sum s is achievable
+            // Restore the result
+            for (int i = 0; i < n; ++i)
             {
-                for (int k = 1; k <= counts[i] && s + k * values[i] <= target; k++)
+                for (int k = 0; k < used_coins[i]; ++k)
                 {
-                    int ns = s + k * values[i];
-                    if (dp[ns] == -1 || dp[ns] > dp[s] + k)
-                    {
-                        dp[ns] = dp[s] + k;
-                        used[ns] = i; // Store the index of the coin
-                    }
+                    result.push_back(values[i]);
+                }
+            }
+            std::sort(result.rbegin(), result.rend());
+            return true;
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            // Try to add from 1 to counts[i] coins of denomination i
+            for (int k = 1; k <= counts[i] - used_coins[i]; ++k)
+            {
+                int new_sum = sum + k * values[i];
+                if (new_sum > target)
+                    break;
+                if (!used[new_sum])
+                {
+                    auto new_used_coins = used_coins;
+                    new_used_coins[i] += k;
+                    used[new_sum] = true;
+                    q.emplace(new_sum, new_used_coins);
                 }
             }
         }
     }
 
-    if (dp[target] == -1)
-        return false;
-
-    // Restore the result
-    int s = target;
-    std::vector<int> used_count(n, 0);
-    while (s > 0)
-    {
-        int coin_index = used[s];
-        used_count[coin_index]++;
-        s -= values[coin_index];
-    }
-
-    for (int i = 0; i < n; ++i)
-    {
-        for (int k = 0; k < used_count[i]; ++k)
-        {
-            result.push_back(values[i]);
-        }
-    }
-
-    std::sort(result.rbegin(), result.rend());
-    return true;
+    return false;
 }
